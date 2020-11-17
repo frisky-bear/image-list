87619b7df43fd43d240ac3416a52f1049c1ea336
frisky-bear <vassil.kurk@gmail.com>
2020-11-17 15:54:14 -0500

    setting state and onChange event handler

--- src/components/SearchInput.js
+++ src/components/SearchInput.js
@@ -2,13 +2,20 @@ import React from 'react';
 
 //class based component because eventually state will be used for user input
 class SearchInput extends React.Component{
+    state = { entry: '' }
+
     render() {
         return(
             <div className="ui segment">
                 <form className="ui form" action="">
                     <div className="field">
                         <div className="ui massive icon input">
-                            <input type="text" placeholder="search" />
+                            <input
+                                type="text"
+                                placeholder="search..."
+                                onChange={(event) => this.setState({entry:event.target.value})}
+                                value={this.state.entry}
+                            />
                             <i className="search icon"></i>
                         </div>
                     </div>
