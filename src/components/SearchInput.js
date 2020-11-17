eb627c30cb0535a0e485e7528d0d6baa88c668a0
frisky-bear <vassil.kurk@gmail.com>
2020-11-17 15:21:14 -0500

    making some minor setup to components like Search and App

---
+++ src/components/SearchInput.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+//class based component because eventually state will be used for user input
+class SearchInput extends React.Component{
+    render() {
+        return(
+            <div>
+                <form action="">
+                    <input type="text" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchInput;
